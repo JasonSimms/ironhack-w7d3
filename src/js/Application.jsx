cbc76243e473cc2ce46d4add5ccba71b11869ee2
Lukas Gisder-Dubé <gisderdube@gmail.com>
2018-09-12 14:58:23 +0200

    add Browser Router

--- src/js/Application.jsx
+++ src/js/Application.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
+import Landing from './Landing'
 import Pokemon from './Pokemon'
 
 class Application extends React.Component {
     render() {
-        return <Pokemon />
+        return (
+            <BrowserRouter>
+                <div>
+                    <Landing />
+                    <Pokemon />
+                </div>
+            </BrowserRouter>
+        )
     }
 }
 
