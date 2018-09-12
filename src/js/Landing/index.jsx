2e4464fbc0d34cef2e3d8ca98e0d279677cb3dd9
Lukas Gisder-Dubé <gisderdube@gmail.com>
2018-09-12 15:32:19 +0200

    added first react router link

--- src/js/Landing/index.jsx
+++ src/js/Landing/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 const index = () => {
     return (
         <div className="container landing">
             <div className="intro">
-                <h1>Welcome to our Pokemon page!</h1>
-                <h3>Try to catch them all.</h3>
+                <h1 className="title">Welcome to our Pokemon page!</h1>
+                <h3 className="subtitle">Try to catch them all.</h3>
+                <Link to="/pokemon">
+                    <button className="button">See all pokemon</button>
+                </Link>
             </div>
         </div>
     )
